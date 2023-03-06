4f82eae2bd80a1bc44146eacaa49b156ea70c6e0
Tri Le <trile95.it@gmail.com>
2023-03-05 20:41:54 -0500

    update mongo db connection uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://favorite-book-list-midterm:DxINHoQt6oqpAjEU@cluster0.pt44h26.mongodb.net/books-comp229-midterm?retryWrites=true&w=majority"
 };
